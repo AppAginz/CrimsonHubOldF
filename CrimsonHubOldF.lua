e1e8ea36018032b970d2942e06ff9ff076d1acfc
Ꭿ₲İ₦Z <72808142+AppAginz@users.noreply.github.com>
2024-06-19 23:39:19 +0300

    Update CrimsonHubOldF.lua

--- CrimsonHubOldF.lua
+++ CrimsonHubOldF.lua
@@ -2,34 +2,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Roboj
 
 local Window = Library.CreateLib("Crimson", "RJTheme4")
 
-local Tab = Window:NewTab("Player")
-local Section = Tab:NewSection("W.I.P.")
-
-local Tab = Window:NewTab("Teleport")
-local Section = Tab:NewSection("Fast Travel")
-Section:NewButton("W.I.P.", "", function()
-    print("Clicked")
-end)
-
-Section:NewButton("W.I.P.", "", function()
-    print("Clicked")
-end)
-
-Section:NewButton("W.I.P.", "", function()
-    print("Clicked")
-end)
-
-Section:NewButton("W.I.P.", "", function()
-    print("Clicked")
-end)
-
 local Tab = Window:NewTab("Farm")
-
-local Section = Tab:NewSection("Buy Items")
-
-Section:NewButton("Requiem Arrow", "", function()
-    print("Clicked")
-end)
 
 local Section = Tab:NewSection("STAND MANUAL FARM")
 Section:NewKeybind("Jesus", "jeez", Enum.KeyCode.Equals, function()
@@ -41,7 +14,7 @@ Section:NewKeybind("Jesus", "jeez", Enum.KeyCode.Equals, function()
 game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
 end)
 
-Section:NewButton("Casino", "$$$", function()
+Section:NewButton("Casino", "", function()
 local args = {
     [1] = "EndDialogue",
     [2] = {
@@ -67,30 +40,6 @@ game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unp
 end)
 
 Section:NewKeybind("Ribka", "ribo4ka", Enum.KeyCode.Insert, function()
-local args = {
-    [1] = "EndDialogue",
-    [2] = {
-        ["Option"] = "Option1",
-        ["Dialogue"] = "Dialogue2",
-        ["NPC"] = "Rib Cage of The Saint's Corpse"
-    }
-}
-
-game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(args))
-end)
-
-
-Section:NewKeybind("TUSK FARM (worth+Rib)", "ee", Enum.KeyCode.Delete, function()
-local args = {
-    [1] = "LearnSkill",
-    [2] = {
-        ["Skill"] = "Worthiness V",
-        ["SkillTreeType"] = "Character"
-    }
-}
-
-game:GetService("Players").LocalPlayer.Character.RemoteFunction:InvokeServer(unpack(args))
-wait(1)
 local args = {
     [1] = "EndDialogue",
     [2] = {
